e27958532f70ffd8cfdbe45f05555d47ddd3db2c
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2019-02-27 15:40:41 -0300

    modificando ConnectionFactory

--- leriadoApp/java/com/ifpb/edu/leriadoApp/App.java
+++ leriadoApp/java/com/ifpb/edu/leriadoApp/App.java
@@ -18,7 +18,7 @@ public class App
         UsuarioDaoImpl dao = new UsuarioDaoImpl();
 //        dao.criar(new Usuario("zeCouves@gmail.com", "MaRiA", "Zé", "das Couves", "M", LocalDate.now(), 1, "40028922", "Rua", "Narnia", "Sólido",  "423", "79311-013"));
 //        dao.remover(12);
-        System.out.println(dao.login("zeCouves@gmail.com", "MaRia"));
+        System.out.println(dao.login("zeCouves@gmail.com", "MaRiA"));
 //        Usuario novoUsuario = new Usuario("leanderson@","123","Leanderson","COelho","M",LocalDate.now(),1,"432423","rua","Cz","PB","2","243");
 //        dao.atualizar(novoUsuario, 2);
 //        System.out.println(dao.buscarPorId(2));
