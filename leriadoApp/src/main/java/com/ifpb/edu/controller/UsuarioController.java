dffd3e5c9b399609897937c702e62b7b0524357e
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-02-28 11:23:56 -0300

    Controller - UsuarioController método execute
    Criação dos cabeçalhos dos métodos do command UsuarioController.

---
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/UsuarioController.java
@@ -0,0 +1,61 @@
+package com.ifpb.edu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ifpb.edu.model.dao.UsuarioDao;
+import com.ifpb.edu.model.dao.UsuarioDaoImpl;
+
+public class UsuarioController implements Command{
+	private UsuarioDao usuarioDao;
+	
+	public UsuarioController() {
+		usuarioDao = new UsuarioDaoImpl();
+	}
+	
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		String acao = request.getParameter("acao");
+		switch(acao) {
+			case "criar":
+				cadastrar(request,response);
+				break;
+			case "remover":
+				remover(request,response);
+				break;
+			case "atualizar":
+				atualizar(request,response);
+			case "buscarPorEmail":
+				buscarPorEmail(request,response);
+				break;
+			case "buscarProId":
+				buscarPorId(request,response);
+				break;
+		}
+	}
+
+	private void buscarPorId(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void buscarPorEmail(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void atualizar(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void remover(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cadastrar(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+}
