3dba8202c6385d5429573fda93f61da7c302af09
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-02-28 10:04:01 -0300

    Controller - FrontController métodos de requisição
    Foi criado o cabeçalho dos métodos de requisição

---
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/FrontController.java
@@ -0,0 +1,28 @@
+package com.ifpb.edu.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet{
+
+	@Override
+	public void doGet(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+	
+	@Override
+	public void doDelete(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+	
+	@Override
+	public void doPut(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+}
