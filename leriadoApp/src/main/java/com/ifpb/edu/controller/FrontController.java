6ebb86470cefdb264d04fdc28dc100dec833f157
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-02-28 11:21:01 -0300

    Controller - FrontController método execute
    Criação do método execute no FrontController.

--- leriadoApp/src/main/java/com/ifpb/edu/controller/FrontController.java
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/FrontController.java
@@ -1,11 +1,29 @@
 package com.ifpb.edu.controller;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/Leriado")
 public class FrontController extends HttpServlet{
-
+	
+	private void execute(HttpServletRequest request,HttpServletResponse response) {
+		try {
+			String commandName = request.getParameter("command");
+			Command command = (Command) Class.forName(this.getClass().getPackage().getName()+"."+commandName).newInstance();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public void doGet(HttpServletRequest request,HttpServletResponse response) {
 		//TODO
@@ -13,7 +31,7 @@ public class FrontController extends HttpServlet{
 	
 	@Override
 	public void doPost(HttpServletRequest request,HttpServletResponse response) {
-		//TODO
+		execute(request,response);		
 	}
 	
 	@Override
