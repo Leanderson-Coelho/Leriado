2c38df617eb424ead837d6581a88ec5529b90e9a
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-02-28 11:22:13 -0300

    Controller - LoginController
    Criação do servlet 'login' e cabeçalho dos métodos.

---
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/LoginController.java
@@ -0,0 +1,30 @@
+package com.ifpb.edu.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("login")
+public class LoginController extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+	
+	@Override
+	public void doPost(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+	
+	@Override
+	public void doDelete(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+	
+	@Override
+	public void doPut(HttpServletRequest request,HttpServletResponse response) {
+		//TODO
+	}
+}
