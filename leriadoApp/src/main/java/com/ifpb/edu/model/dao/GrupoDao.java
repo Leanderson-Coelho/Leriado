1dcef5d532c1b8a859d83d0d85a61da240552755
Ian-Carneiro <ianteixeiraaraujo@gmail.com>
2019-02-27 21:51:23 -0300

    adicionando throws SQLException em metodos da interface GrupoDao

--- leriadoApp/src/main/java/com/ifpb/edu/model/dao/GrupoDao.java
+++ leriadoApp/src/main/java/com/ifpb/edu/model/dao/GrupoDao.java
@@ -7,7 +7,7 @@ import com.ifpb.edu.model.domain.Grupo;
 
 public interface GrupoDao {
 	public void criar(Grupo novoGrupo) throws SQLException;
-	public void excluir(int idGrupo);
+	public void excluir(int idGrupo) throws SQLException;
 	void adicionarUsuario(int idGrupo, int idUsuario) throws SQLException;
-	public void removerUsuario(String email);
+	public void removerUsuario(String email) throws SQLException;
 }
