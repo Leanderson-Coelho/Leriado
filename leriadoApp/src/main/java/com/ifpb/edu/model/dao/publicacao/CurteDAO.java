498c26055aee9f460a745214173f4b048258976e
Isleimar Oliveira <isleimar@hotmail.com>
2019-03-03 13:29:01 -0300

    Criação da classe CurteDAOImpDB

--- leriadoApp/src/main/java/com/ifpb/edu/model/dao/publicacao/CurteDAO.java
+++ leriadoApp/src/main/java/com/ifpb/edu/model/dao/publicacao/CurteDAO.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import com.ifpb.edu.model.domain.publicacao.Curte;
 import com.ifpb.edu.model.domain.publicacao.Texto;
+import com.ifpb.edu.model.jdbc.DataAccessException;
 
 public interface CurteDAO {
 	
-	int criaCurte(Curte curte);
-	void editaCurte(Curte curte);
-	void excluiCurte(Curte curte);
-	int quantCurtidas(Texto texto);
-	List<Curte> listaCurte(Texto texto);
+	void criaCurte(Curte curte) throws DataAccessException;
+	void editaCurte(Curte curte) throws DataAccessException;
+	void excluiCurte(Curte curte) throws DataAccessException;
+	int quantCurtidas(Texto texto) throws DataAccessException;
+	List<Curte> listaCurte(Texto texto) throws DataAccessException;
 }
---
+++ leriadoApp/src/main/java/com/ifpb/edu/model/dao/publicacao/impdb/CurteDAOImpDB.java
@@ -0,0 +1,62 @@
+package com.ifpb.edu.model.dao.publicacao.impdb;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+import com.ifpb.edu.model.dao.publicacao.CurteDAO;
+import com.ifpb.edu.model.domain.publicacao.Curte;
+import com.ifpb.edu.model.domain.publicacao.Texto;
+import com.ifpb.edu.model.jdbc.ConnectionFactory;
+import com.ifpb.edu.model.jdbc.DataAccessException;
+
+public class CurteDAOImpDB implements CurteDAO{
+	
+	private Connection connection;
+		
+	public CurteDAOImpDB() {
+		connection = ConnectionFactory.getInstance().getConnection();
+	}
+
+	@Override
+	public void criaCurte(Curte curte) throws DataAccessException{
+		try {
+			String query = "INSERT INTO curte (textoid,usuarioid) "
+					+ "VALUES(?,?)";
+			PreparedStatement stm =  connection.prepareStatement(query);
+			stm.setInt(1,curte.getTexto().getId());
+			stm.setInt(2,curte.getUsuario().getId());			
+			stm.executeUpdate();
+			}catch (Exception e) {
+				throw new DataAccessException("Falha ao criar curtida");
+			}		
+	}
+
+	@Override
+	public void editaCurte(Curte curte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluiCurte(Curte curte) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int quantCurtidas(Texto texto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<Curte> listaCurte(Texto texto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
