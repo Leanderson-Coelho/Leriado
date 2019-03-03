11ff69ca740caa890c13154c03085afc235c70ef
Isleimar Oliveira <isleimar@hotmail.com>
2019-03-02 23:17:49 -0300

    Modificação do procedimento buscar da interface TextoDAO.

--- leriadoApp/src/main/java/com/ifpb/edu/model/dao/publicacao/TextoDAO.java
+++ leriadoApp/src/main/java/com/ifpb/edu/model/dao/publicacao/TextoDAO.java
@@ -1,6 +1,7 @@
 package com.ifpb.edu.model.dao.publicacao;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.ifpb.edu.model.domain.publicacao.Texto;
 import com.ifpb.edu.model.jdbc.DataAccessException;
@@ -9,7 +10,7 @@ public interface TextoDAO {
 	int cria(Texto texto) throws DataAccessException;
 	void edita(Texto texto) throws DataAccessException;
 	void exclui(Texto texto) throws DataAccessException;
-	Texto buscar(int id) throws DataAccessException;
+	Optional<Texto> buscar(int id) throws DataAccessException;
 	TipoTexto tipo(Texto texto) throws DataAccessException;
 	List<Texto> lista() throws DataAccessException;
 	List<Texto>lista(int inicio,int quant) throws DataAccessException;
