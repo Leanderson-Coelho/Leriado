825c7cc8cd1964e721d98096cade484ec384eb27
Isleimar Oliveira <isleimar@hotmail.com>
2019-03-04 10:27:54 -0300

    Criação da classe MarcaDAOImpDB

---
+++ leriadoApp/src/main/java/com/ifpb/edu/model/dao/publicacao/impdb/MarcaDAOImpDB.java
@@ -0,0 +1,35 @@
+package com.ifpb.edu.model.dao.publicacao.impdb;
+
+import java.util.List;
+
+import com.ifpb.edu.model.dao.publicacao.MarcaDAO;
+import com.ifpb.edu.model.domain.publicacao.Marca;
+import com.ifpb.edu.model.domain.publicacao.Texto;
+
+public class MarcaDAOImpDB implements MarcaDAO{
+
+	@Override
+	public int criaMarca(Marca marca) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void editaMarca(Marca marca) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluiMarca(Marca marca) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Marca> listaMarca(Texto texto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
