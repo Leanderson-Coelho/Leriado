45c9fe71b815d3f2d83781951ac9db1f7a7b077a
Isleimar Oliveira <isleimar@hotmail.com>
2019-03-13 15:59:00 -0300

    Criação da classe PublicacaoController

---
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/publicacao/PublicacaoController.java
@@ -0,0 +1,22 @@
+package com.ifpb.edu.controller.publicacao;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ifpb.edu.controller.Command;
+import com.ifpb.edu.controller.exception.CommandException;
+
+public class PublicacaoController implements Command{
+	
+	public PublicacaoController() {
+		
+	}
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws CommandException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
