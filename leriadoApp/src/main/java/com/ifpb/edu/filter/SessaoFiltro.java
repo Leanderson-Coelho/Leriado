9df7a94e41be77ecd5af4693f1e3a6a5a1aa0444
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-03-15 11:30:04 -0300

    Filter - Classe SessaoFilter
    Criando classe SessaoFilter e implementando.
    A mesma garante que o usuário logado não acesse
    a página inicial.

---
+++ leriadoApp/src/main/java/com/ifpb/edu/filter/SessaoFiltro.java
@@ -0,0 +1,39 @@
+package com.ifpb.edu.filter;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName="SessaoFilter", urlPatterns="/index.jsp")
+public class SessaoFiltro extends HttpFilter{
+	private static Logger log = Logger.getLogger("SessaoFilter");
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void filtrarRequisicao(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if(request.getSession().getAttribute("usuarioLogado") != null) {
+			log.severe("--> Requisição inválida");
+			response.sendRedirect("restrito/logado.jsp");
+		}
+		limparHeader(response);
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+}
