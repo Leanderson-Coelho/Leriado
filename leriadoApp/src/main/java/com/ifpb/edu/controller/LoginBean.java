88ad1d36732acddb099503e59762a06d1325c638
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-04-02 16:19:43 -0300

    Erro no lign, compartilhando no repositorio para os demais ajudar.

---
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/LoginBean.java
@@ -0,0 +1,61 @@
+package com.ifpb.edu.controller;
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.ifpb.edu.model.dao.UsuarioDao;
+import com.ifpb.edu.model.dao.UsuarioDaoImpl;
+import com.ifpb.edu.model.domain.Usuario;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+	UsuarioDao usuarioDao;
+	
+	Usuario usuarioLogado;
+	
+	String login;
+	String senha;
+	
+	@PostConstruct
+	public void init() {
+		usuarioDao = new UsuarioDaoImpl();
+	}
+	
+	public String verificacaoLogin() throws Exception {
+		if(usuarioDao.login(login, senha)) {
+			usuarioLogado = usuarioDao.buscarPorEmail(login);
+			return "inicio";
+		}else {
+			return "login";
+		}
+	}
+
+	public Usuario getUsuarioLogado() {
+		return usuarioLogado;
+	}
+
+	public void setUsuarioLogado(Usuario usuarioLogado) {
+		this.usuarioLogado = usuarioLogado;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+}
