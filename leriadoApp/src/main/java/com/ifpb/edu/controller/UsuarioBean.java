215338ca447b2e5996b0bc39319d8bca250d74ea
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-04-02 21:40:32 -0300

    UsuarioBean - Função de excluir conta
    Foi implementado o métdodo de remover a conta do usuário
    que está logado, a função se encontra no UsuariBean.

--- leriadoApp/src/main/java/com/ifpb/edu/controller/UsuarioBean.java
+++ leriadoApp/src/main/java/com/ifpb/edu/controller/UsuarioBean.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 
 import com.ifpb.edu.model.dao.UsuarioDao;
@@ -16,6 +17,10 @@ public class UsuarioBean {
 	private Usuario usuario;
 	private UsuarioDao usuarioDao;
 	private String teste = "TESTE DE BEAN";
+	
+	@ManagedProperty("#{loginBean}")
+	private LoginBean loginBean;
+	
 	@PostConstruct
 	public void init() {
 		usuarioDao = new UsuarioDaoImpl();
@@ -29,6 +34,11 @@ public class UsuarioBean {
 		usuarioDao.criar(usuario);
 		return "inicio";
 	}
+	
+	public String excluirConta() throws SQLException {
+		usuarioDao.remover(loginBean.getUsuarioLogado().getId());
+		return "login";
+	}
 
 	public Usuario getUsuario() {
 		return usuario;
@@ -37,4 +47,14 @@ public class UsuarioBean {
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+	
+
+	public LoginBean getLoginBean() {
+		return loginBean;
+	}
+
+	public void setLoginBean(LoginBean loginBean) {
+		this.loginBean = loginBean;
+	}
+	
 }
